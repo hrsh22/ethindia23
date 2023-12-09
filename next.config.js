8f8333349725f4ddf7edaa717812d9f22cc753cd
Harsh <harsh.gupta2211@gmail.com>
2023-12-09 17:44:22 +0530

    PUSHHHH

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 // in next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  // webpack: (
+  //   config,
+  //   { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  // ) => {
+  //   config.module.rules.push({
+  //     test: /\.mjs$/,
+  //     include: /node_modules/,
+  //     type: "javascript/auto",
+  //   });
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
